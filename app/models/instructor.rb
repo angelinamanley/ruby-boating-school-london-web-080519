e7bbceae3f8f5dd3807dcc07198e7b26ad191ca6
github username <github email address>
2019-08-10 19:50:58 +0100

    Done

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,36 @@
 class Instructor
 
+    attr_reader :name
+
+    @@all = []
+
+    def initialize(name)
+        @name = name 
+        @@all << self 
+    end 
+
+    def self.all 
+        @@all 
+    end 
+
+    def pass_student(student, test)
+        BoatingTest.all.each do |test|
+            if test.student == student && test.test_name == test
+                test.status = "passed"
+            else 
+                BoatingTest.new(student, test, "passed", self)
+            end 
+        end 
+    end 
+
+    # def fail_student(student, test)
+    #     BoatingTest.all.each do |test|
+    #         if test.student == student && test.test_name == test
+    #             test.status = "failed"
+    #         else 
+    #             BoatingTest.new(student, test, "failed", self)
+    #         end 
+    #     end 
+    # end
+
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,34 @@
 class Student
 
+    attr_reader :name
+
+    @@all = []
+    def initialize(name)
+        @name = name 
+        @@all << self
+    end 
+
+    def self.all 
+        @@all 
+    end 
+
+    def add_boating_test(test, status, instructor)
+        BoatingTest.new(self, test, status, instructor)
+    end 
+
+    def self.find_student(name)
+        Student.all.find {|student| student.name == name}
+    end 
+
+    def grade_percentage
+        tests_taken = BoatingTest.all.select {|test| test.student == self}
+        tests_passed = tests_taken.select { |test| test.status == "passed"}.count.to_f
+        total_tests = tests_taken.count.to_f
+        percentage = tests_passed / total_tests * 100 
+        
+    end
+    
+     
+     
+
 end
